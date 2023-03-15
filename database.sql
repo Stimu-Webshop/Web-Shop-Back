4842b97ed66ed1fc712c61dcade885e21ecb6a04
saruot <ruotsalainen.samuli@gmail.com>
2023-03-15 10:26:54 +0200

    Korjattu toimivat luontilauseet tietokantaan

--- database.sql
+++ database.sql
@@ -5,40 +5,32 @@ create database stimu;
 use stimu;
 
 
+create table product_category (
+    id int auto_increment,
+    name varchar(255) not null,
+    description text,
+    PRIMARY KEY (id)
+);
+
+create table product_inventory (
+    id INT AUTO_INCREMENT,
+    quantity INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
 create table product (
     id int primary key auto_increment,
     name varchar(255) not null,
     description text not null,
     img varchar(255) not null,
     price decimal(10,2) not null,
-    category_id int foreign key not null,
-    inventory_id int foreign key not null
+    category_id int not null,
+    inventory_id int not null,
+    FOREIGN KEY (category_id) REFERENCES product_category(id),
+    FOREIGN KEY (inventory_id) REFERENCES product_inventory(id)
 
-)
-
-create table product_category (
-    id int primary key auto_increment,
-    name varchar(255) not null,
-    desc text
-)
-
-create table product_inventory (
-    id int primary key auto_increment,
-    quantity int not null
-)
-
-create table shopping_cart (
-    id int primary key auto_increment,
-    session_id int foreign key not null,
-    product_id int foreign key not null,
-    quantity int not null
-)
-
-create table shopping_session (
-    id int primary key auto_increment,
-    user_id int foreign key not null,
-    total decimal(10,2) not null,
-)
+);
 
 create table user (
     id int primary key auto_increment,
@@ -52,15 +44,35 @@ create table user (
     city varchar(255) not null,
     postal_code varchar(255) not null,
     country varchar(255) not null
-)
+);
+
+create table shopping_session (
+    id int primary key auto_increment,
+    user_id int not null,
+    total decimal(10,2) not null,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+create table shopping_cart (
+    id int primary key auto_increment,
+    session_id int not null,
+    product_id int  not null,
+    quantity int not null,
+    FOREIGN KEY (session_id) REFERENCES shopping_session(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+
+
+
 
 create table admin_type (
     id int primary key auto_increment,
     admin_type varchar(255) not null,
     permissions varchar(255) not null
-)
+);
 
-)
+
 
 create table admin_user (
     id int primary key auto_increment,
@@ -68,8 +80,9 @@ create table admin_user (
     password varchar(255) not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    type_id int foreign key not null
-)
+    type_id int not null,
+    FOREIGN KEY (type_id) REFERENCES admin_type(id)
+);
 
 
 
