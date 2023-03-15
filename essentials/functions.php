d282de0815f2c5d49f93a9bdc9c3e34d900d3c66
saruot <ruotsalainen.samuli@gmail.com>
2023-03-15 10:45:33 +0200

    Reconfig and modified dbopen function

--- essentials/functions.php
+++ essentials/functions.php
@@ -5,9 +5,13 @@ function openDb(): object {
   $database = $ini['database'];
   $user = $ini['user'];
   $password = $ini['password'];
-  $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
-  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  return $db;
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
+        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return $db;
+    } catch (PDOException $pdoex) {
+        returnError($pdoex);
+    }
 } 
 
 function returnError(PDOException $pdoex): void {
