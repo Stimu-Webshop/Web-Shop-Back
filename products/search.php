1dff23d58ca94b1c2442949677629210bc8b1a4a
Serveriii <n2jose00@students.oamk.fi>
2023-03-20 13:26:43 +0200

    Search bar php

--- products/search.php
+++ products/search.php
@@ -1,14 +1,13 @@
 <?php
 
-// Replace the database credentials with your own
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database_name";
+require_once '../essentials/functions.php';
+require_once '../essentials/headers.php';
+
+
 
 try {
   // Create connection
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn = openDb();
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -16,7 +15,7 @@ try {
   $search_query = $conn->quote('%' . $_GET['q'] . '%');
 
   // Query to retrieve data from the database based on the search query
-  $sql = "SELECT * FROM products WHERE name LIKE $search_query";
+  $sql = "SELECT * FROM product WHERE name LIKE $search_query";
 
   // Prepare the statement
   $stmt = $conn->prepare($sql);
