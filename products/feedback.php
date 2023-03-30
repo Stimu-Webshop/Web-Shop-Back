3ca976e8dce27f674564853ed3880081302a9ffc
n2jato00 <n2jato00@students.oamk.fi>
2023-03-30 12:54:18 +0300

    updated version of feedback.php

--- products/feedback.php
+++ products/feedback.php
@@ -2,34 +2,41 @@
 require_once '../essentials/functions.php';
 require_once '../essentials/headers.php';
 
-if (isset($_POST['submit'])) {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+// Get the raw POST data
+$data = file_get_contents('php://input');
 
-    $dsn = openDb();
+// Parse the JSON-encoded data into an associative array
+$contactData = json_decode($data, true);
 
-    try {
-        $query = "INSERT INTO contact_form (first_name, last_name, email, address, phone, message) VALUES (:first_name, :last_name, :email, :address, :phone, :message)";
+// Extract the fields from the associative array
+$firstName = $contactData['firstName'];
+$lastName = $contactData['lastName'];
+$email = $contactData['email'];
+$address = $contactData['address'];
+$phone = $contactData['phone'];
+$message = $contactData['message'];
 
-        $stmt = $dsn->prepare($query);
+// Connect to the database
+$dsn = openDb();
 
-        $stmt->bindParam(':first_name', $first_name, PDO::PARAM_STR);
-        $stmt->bindParam(':last_name', $last_name, PDO::PARAM_STR);
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':address', $address, PDO::PARAM_STR);
-        $stmt->bindParam(':phone', $phone, PDO::PARAM_STR);
-        $stmt->bindParam(':message', $message, PDO::PARAM_STR);
+// Insert the data into the contact_form table
+try {
+    $query = "INSERT INTO contact_form (first_name, last_name, email, address, phone, message) VALUES (:firstName, :lastName, :email, :address, :phone, :message)";
 
-        $stmt->execute();
+    $stmt = $dsn->prepare($query);
 
-        echo "Kiitos viestistäsi!";
+    $stmt->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+    $stmt->bindParam(':lastName', $lastName, PDO::PARAM_STR);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':address', $address, PDO::PARAM_STR);
+    $stmt->bindParam(':phone', $phone, PDO::PARAM_STR);
+    $stmt->bindParam(':message', $message, PDO::PARAM_STR);
 
-    } catch (PDOException $e) {
-        echo "Virhe tietokantaan tallennettaessa: " . $e->getMessage();
-    }
+    $stmt->execute();
+
+    echo "Kiitos viestistäsi!";
+
+} catch (PDOException $e) {
+    echo "Virhe tietokantaan tallennettaessa: " . $e->getMessage();
 }
 ?>
