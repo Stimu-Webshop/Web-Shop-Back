b4c726cf096a9b44d24195fcac0596fca369b0bd
saruot <ruotsalainen.samuli@gmail.com>
2023-03-30 14:30:38 +0300

    Almost working accountdata update function

--- accounts/updateaccountdata.php
+++ accounts/updateaccountdata.php
@@ -2,15 +2,17 @@
 require_once '../essentials/functions.php';
 require_once '../essentials/headers.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// ONGELMA : EI SAA OIKEAA USERIDTÄ FRONTENDILTÄ
+// PALAUTTAA ARVON NULL
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $db = openDb();
     $userId = json_decode($_GET['userId'], true);
+    $userIdNum = $data['userId'];
     $requestData = file_get_contents('php://input');
-    $userData = json_decode($requestData, true);
+    $userData = array_filter(json_decode($requestData, true));
 
-    error_log("requestData: $requestData");
-    error_log("userData: " . print_r($userData, true));
-
+    var_dump($userId);
     $userData = array(
         'username' => $userData['username'] ?? null,
         'first_name' => $userData['first_name'] ?? null,
@@ -25,73 +27,71 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'new_password' => $userData['new_password'] ?? null
     );
 
-
-    // Populate userData array with only non-empty fields
+    // Populate non-empty userData array
+    $nonEmptyUserData = array();
     foreach ($userData as $key => $value) {
         if (isset($userData[$key]) && !empty($userData[$key])) {
-          $userData[$key] = $value;
+            $nonEmptyUserData[$key] = $value;
         }
-      }
-
-    var_dump($userData);
-
-    error_log("userData after populating: " . print_r($userData, true));
+    }
 
     // Check if username already exists
-    if (!empty($userData['username'])) {
-      $stmt = $db->prepare('SELECT id FROM user WHERE username = :username');
-      $stmt->execute(array(':username' => $userData['username']));
-      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!empty($nonEmptyUserData['username'])) {
+        $stmt = $db->prepare('SELECT id FROM user WHERE username = :username');
+        $stmt->execute(array(':username' => $nonEmptyUserData['username']));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
         echo 'Checked for username';
-      if ($row && $row['id'] != $userId) {
-        http_response_code(409);
-        echo json_encode(array('error' => 'Username already exists.'));
-        exit();
-      }
+        if ($row && $row['id'] != $userId) {
+            http_response_code(409);
+            echo json_encode(array('error' => 'Username already exists.'));
+            exit();
+        }
     }
 
     // Check if current password is correct before updating it
-    if (!empty($userData['current_password'])) {
-      $stmt = $db->prepare('SELECT password FROM user WHERE id = :userId');
-      $stmt->execute(array(':userId' => $userId));
-      $row = $stmt->fetch(PDO::FETCH_ASSOC);
-      echo 'Checked for password';
-      if (!password_verify($userData['current_password'], $row['password'])) {
-        http_response_code(401);
-        echo json_encode(array('error' => 'Invalid password.'));
-        exit();
-      }
+    if (!empty($nonEmptyUserData['current_password'])) {
+        $stmt = $db->prepare('SELECT password FROM user WHERE id = :userId');
+        $stmt->execute(array(':userId' => $userId));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo 'Checked for password';
+        if (!password_verify($nonEmptyUserData['current_password'], $row['password'])) {
+            http_response_code(401);
+            echo json_encode(array('error' => 'Invalid password.'));
+            exit();
+        }
 
-      // Hash and update new password if present
-      if (!empty($userData['new_password'])) {
-        $hashedPassword = password_hash($userData['new_password'], PASSWORD_DEFAULT);
-        $stmt = $db->prepare('UPDATE users SET password = :password WHERE id = :userId');
-        $stmt->execute(array(':password' => $hashedPassword, ':userId' => $userId));
-        echo 'Updated password';
-    }
+        // Hash and update new password if present
+        if (!empty($nonEmptyUserData['new_password'])) {
+            $hashedPassword = password_hash($nonEmptyUserData['new_password'], PASSWORD_DEFAULT);
+            $stmt = $db->prepare('UPDATE user SET password = :password WHERE id = :userId');
+            $stmt->execute(array(':password' => $hashedPassword, ':userId' => $userId));
+            echo 'Updated password';
+        }
 
-      unset($userData['current_password'], $userData['new_password']);
-    }
-    echo 'Before updating';
-    // Update remaining fields
-    $set = array();
-    foreach ($userData as $key => $value) {
-      if (!is_null($value)) {
-        $set[] = "$key = :$key";
-      }
-    }
-   
-	var_dump($userId);
-	var_dump($set);
-	echo(count($set));
-    
-    if (count($set) > 0) {
-        $sql = "UPDATE user SET " . implode(",", $set) . " WHERE id = :userId";
-        $stmt = $db->prepare($sql);
-        $userData['userId'] = $userId;
-        $stmt->execute($userData);
-        echo 'actually updated data';
-      }
+        // Remove password fields from non-empty userData array
+        unset($nonEmptyUserData['current_password'], $nonEmptyUserData['new_password']);
     }
 
-	echo 'Didnt update';
+// Update remaining fields
+
+$set = array();
+
+$params = array();
+foreach ($nonEmptyUserData as $key => $value) {
+    $set[] = "$key = :$key";
+    $params[":$key"] = $value;
+}
+var_dump($params);
+
+if (count($set) > 0) {
+    $sql = "UPDATE user SET " . implode(",", $set) . " WHERE id = :userId";
+    $stmt = $db->prepare($sql);
+    $params['userId'] = $userIdNum;
+    $stmt->execute($params);
+    $rowCount = $stmt->rowCount();
+    echo $sql;
+    echo "Number of affected rows: $rowCount";
+} else {
+    echo 'No fields to update';
+}
+}
