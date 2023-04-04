2e4af834e2a66a4abc88f77309ed6d3c3a9e18b3
saruo <ruotsalainen.samuli@gmail.com>
2023-04-04 07:37:38 +0300

    Updated table creation syntaxes

--- database.sql
+++ database.sql
@@ -105,3 +105,41 @@ create table contact_form (
 )
 
 
+TUOTELUOKKIEN LUONTILAUSEET
+
+/* INSERT INTO product_category (id, name, description) VALUES
+(1, Energiajuomat, Nämä ovat energiajuomia),
+(2, Kahvit, Nämä ovat kahveja),
+(3, Sekalainen, Nämä ovat sekalaisia);
+*/
+
+
+
+ -- ENERGIAJUOMIEN LUONTILAUSEET 
+
+INSERT INTO product (id, name, description, img, price, category_id, inventory_id)
+VALUES
+(1,'Velocity Vibe','Kiihdytä vauhtiasi Velocity Vibe -juoman avulla, jonka avulla voit välttää hidasteet tiellä menestykseen.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/energydrink1.png',4.99,1,1)
+(2,'Hyper Drive','Kokeile Hyper Driven voimaa, joka antaa sinulle nopean ja tehokkaan energiapotkun, jotta voit saavuttaa tavoitteesi helposti ja nopeasti.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/energydrink2.png',4.49,1,2),
+(3,'Power Surge','Koe energian voima, joka antaa sinulle voimaa selviytyä pitkistä päivistä ja haastavista tehtävistä.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/Energydrink.png',4.99,1,3),
+(4,'Thunderbolt Fuel','Kun tarvitset nopean ja tehokkaan annoksen energiaa, ThunderBolt Fuel on valintasi. Se antaa sinulle salamannopean vauhdin ja energian, joka kestää tuntikausia.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/energydrink4.png',3.99,1,4),
+(5,'Electric Rush','Tämä sähköisesti latautunut juoma saa sinut heräämään henkiin, virittäen sinut päivän haasteisiin.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/energydrink5.png',2.99,1,5),
+(6,'Atomic Energy','Tämä energiajuoma antaa sinulle ydinpommin kaltaisen voiman, joka pitää sinut täydessä käynnissä pitkän päivän ajan.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/energydrink6.png',7.99,6,1);
+
+
+-- KAHVIEN LUONTILAUSEET
+
+INSERT INTO product (id, name, description, img, price, category_id, inventory_id)
+VALUES
+(7,'Porvari___kahavi','Kirjaimellista kultaa','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/coffeebeans.png',99.99,2,7),
+(8,'Näyttää löftberiltä','Varmaan yhtä pahaakin','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/coffeebeans2.png',1.49,2,8),
+(9,'Alissaan','Kolmannen paahtoasteen suussasulavan pehmeä Arabialainen kahvi.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/alissan-kahvi.png',6.99,2,9),
+(10,'Maststim','Stimuloi muutakin kuin aivoja','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/maststim-kahvi.png',9.99,2,10),
+(11,'Bariul','Kerätty verkkosivuntekijän omin pikku kätösin.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/Bariul%20coffee.png',29.99,2,11),
+(12,'FaaLLL','Tällä kahvilla tipahtaa isompikin kaveri.','',24.99,2,12),
+(13,'Pikku ryssä','Paketin tuoteseloste on venäjäksi, ja sivun tekijä ei puhu sitä. Todennäköisesti ei myrkyllistä.','https://www.students.oamk.fi/~n2rusa00/Stimu/Stimu%20product%20pictures/coffeebeans3.png',4.99,2,13);
+
+
+-- SEKALAISTEN LUONTILAUSEET
+
+  	
